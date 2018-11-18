3979242b2b8e5d16e639989d2a27c0caa68e4388
Andre Luiz da Silva <andreluiz1013@gmail.com>
2018-11-18 15:59:55 -0200

    Atualizando e removendo anexo

--- src/main/java/com/algamoney/api/storage/S3.java
+++ src/main/java/com/algamoney/api/storage/S3.java
@@ -9,12 +9,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.algamoney.api.config.property.AlgamoneyApiProperty;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.AccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.DeleteObjectsResult.DeletedObject;
 import com.amazonaws.services.s3.model.GroupGrantee;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.ObjectTagging;
@@ -90,8 +93,23 @@ public class S3 {
 		amazonS3.setObjectTagging(setObjectTaggingRequest);
 	}
 	
+	public void remover(String anexo) {
+		DeleteObjectRequest deleteObjectRequest =  new DeleteObjectRequest(property.getS3().getBucket(), anexo);
+		amazonS3.deleteObject(deleteObjectRequest);
+	}
+	
+	public void substituir(String objetoAntigo, String objetoNovo) {
+		if (StringUtils.hasText(objetoAntigo)) {
+			this.remover(objetoAntigo);
+		}
+		
+		salvar(objetoNovo);		
+	}
+	
 	private String gerarNomeUnico(String originalFilename) {
 		return UUID.randomUUID().toString() + "_" + originalFilename;
 	}
+
+
 
 }
